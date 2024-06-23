928a1c3776320911471d39b8af0beac2c97122a0
Yuxiang-Huang <yuxiangh142857@gmail.com>
2024-06-23 19:04:16 -0400

    Added mostly functioning clerk

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,11 @@
 import type { Metadata } from 'next';
+import {
+  ClerkProvider,
+  SignInButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from '@clerk/nextjs';
 import './globals.css';
 
 export const metadata: Metadata = {
@@ -15,13 +22,26 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <head>
-        <script>const global = globalThis;</script>
-      </head>
-      <body>
-        <div id="root">{children}</div>
-      </body>
-    </html>
+    <ClerkProvider
+      publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}
+    >
+      <html lang="en">
+        <head>
+          <script>const global = globalThis;</script>
+        </head>
+
+        <body>
+          <header>
+            <SignedOut>
+              <SignInButton />
+            </SignedOut>
+            <SignedIn>
+              <UserButton />
+            </SignedIn>
+          </header>
+          <div id="root">{children}</div>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
