7bf882cc6a1a1fc98fa5d7a6b2371f77eb827bca
kzhao214 <kzhao214@gmail.com>
2024-07-13 20:34:22 -0400

    Changed padding and sizes of icons

--- src/components/searchbar/QuickSearch.tsx
+++ src/components/searchbar/QuickSearch.tsx
@@ -27,7 +27,7 @@ export default function QuickSearch({
     return (
       <Collapsible
         trigger={
-          <div className="flex flex-row items-center justify-between px-3 py-2">
+          <div className="flex flex-row items-center justify-between px-2.5 py-2">
             <p className="m-0 font-bold text-black">{title}</p>
             <div>
               {!open ? (
@@ -66,10 +66,10 @@ export default function QuickSearch({
         className="flex w-full flex-col items-center gap-1 rounded-xl p-2"
       >
         <div
-          className="flex h-16 w-16 items-center justify-center rounded-full"
+          className="flex h-14 w-14 items-center justify-center rounded-full"
           style={{ backgroundColor: color }}
         >
-          <Image alt={name + ' icon'} src={icon} className="h-9 w-9"></Image>
+          <Image alt={name + ' icon'} src={icon} className="h-8 w-8"></Image>
         </div>
         <p className="m-0 text-sm font-normal text-[#8e8e8e]">{name}</p>
       </div>
@@ -78,7 +78,7 @@ export default function QuickSearch({
 
   return (
     <CollapsibleSearch title="Quick Search">
-      <div className="w-var(--search-width-desktop) mx-3 mb-3 grid grid-cols-4 gap-2 rounded-xl border border-[#dddddd] bg-white p-2">
+      <div className="w-var(--search-width-desktop) mx-2.5 mb-3 grid grid-cols-4 gap-2 rounded-xl border border-[#dddddd] bg-white p-2">
         {renderHelper('Restroom', toiletIcon, 'Restroom', '#efb1f4')}
         {renderHelper('Coffee', coffeeIcon, 'Cafes', '#ffd66b')}
         {renderHelper('Food', foodIcon, 'Dining', '#ffbd59')}
