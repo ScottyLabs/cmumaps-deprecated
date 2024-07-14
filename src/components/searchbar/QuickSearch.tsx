2b80a8b6ca3f6ed0792a47f3f216ce251b97415a
kzhao214 <kzhao214@gmail.com>
2024-07-13 20:27:01 -0400

    Made quick search drop down and colored icons

--- src/components/searchbar/QuickSearch.tsx
+++ src/components/searchbar/QuickSearch.tsx
@@ -1,10 +1,12 @@
-import React, { ReactElement } from 'react';
+import React, { ReactElement, useState } from 'react';
 import Image from 'next/image';
 import toiletIcon from '/public/assets/icons/toilet.svg';
 import coffeeIcon from '/public/assets/icons/coffee.svg';
 import fountainIcon from '/public/assets/icons/water.svg';
 import foodIcon from '/public/assets/icons/forkknife.svg';
 import { StaticImport } from 'next/dist/shared/lib/get-img-props';
+import Collapsible from 'react-collapsible';
+import { ChevronUpIcon, ChevronDownIcon } from '@heroicons/react/24/solid';
 export interface QuickSearchProps {
   setQuery: (q: string) => void;
 }
@@ -15,35 +17,73 @@ export interface QuickSearchProps {
 export default function QuickSearch({
   setQuery,
 }: QuickSearchProps): ReactElement {
+  interface CollapsibleSearchProps {
+    title: string;
+    children: React.ReactElement;
+  }
+
+  const CollapsibleSearch = ({ title, children }: CollapsibleSearchProps) => {
+    const [open, setOpen] = useState(false);
+    return (
+      <Collapsible
+        trigger={
+          <div className="flex flex-row items-center justify-between px-3 py-2">
+            <p className="m-0 font-bold text-black">{title}</p>
+            <div>
+              {!open ? (
+                <ChevronDownIcon className="h-4 w-4" />
+              ) : (
+                <ChevronUpIcon className="h-4 w-4" />
+              )}
+            </div>
+          </div>
+        }
+        className="rounded-xl bg-white"
+        openedClassName="rounded-xl bg-white"
+        onOpening={() => {
+          setOpen(true);
+        }}
+        onClosing={() => {
+          setOpen(false);
+        }}
+        transitionTime={200}
+        easing="ease-in-out"
+      >
+        {children}
+      </Collapsible>
+    );
+  };
+
   const renderHelper = (
     name: string,
     icon: StaticImport,
     queryText: string,
+    color: string,
   ) => {
     return (
       <div
         onClick={() => setQuery(queryText)}
-        className="flex w-full flex-col items-center rounded-xl bg-[#e1e1e1] px-2 pb-2 pt-0"
+        className="flex w-full flex-col items-center gap-1 rounded-xl p-2"
       >
-        <p className="m-0 font-semibold">{name}</p>
-        <div className="flex h-16 w-16 items-center justify-center rounded-full bg-white">
-          <Image alt={name + ' icon'} src={icon} className=""></Image>
+        <div
+          className="flex h-16 w-16 items-center justify-center rounded-full"
+          style={{ backgroundColor: color }}
+        >
+          <Image alt={name + ' icon'} src={icon} className="h-9 w-9"></Image>
         </div>
+        <p className="m-0 text-sm font-normal text-[#8e8e8e]">{name}</p>
       </div>
     );
   };
 
   return (
-    <div className="rounded-2xl bg-[#8f8f8f] p-2 pb-3">
-      <p className="mb-2 font-bold text-white">Quick Search</p>
-      <div className="w-var(--search-width-desktop) grid grid-cols-4 gap-2 rounded-xl bg-[#b2b2b2] p-2">
-        {renderHelper('Restroom', toiletIcon, 'Restroom')}
-        {renderHelper('Coffee', coffeeIcon, 'Cafes')}
-        {renderHelper('Food', foodIcon, 'Dining')}
-        {renderHelper('Fountain', fountainIcon, 'Fountain')}
+    <CollapsibleSearch title="Quick Search">
+      <div className="w-var(--search-width-desktop) mx-3 mb-3 grid grid-cols-4 gap-2 rounded-xl border border-[#dddddd] bg-white p-2">
+        {renderHelper('Restroom', toiletIcon, 'Restroom', '#efb1f4')}
+        {renderHelper('Coffee', coffeeIcon, 'Cafes', '#ffd66b')}
+        {renderHelper('Food', foodIcon, 'Dining', '#ffbd59')}
+        {renderHelper('Fountain', fountainIcon, 'Fountain', '#52a2ff')}
       </div>
-      <p className="my-2 font-bold text-white">Events</p>
-      <div className="h-16 w-full rounded-lg bg-[#b2b2b2]"></div>
-    </div>
+    </CollapsibleSearch>
   );
 }
