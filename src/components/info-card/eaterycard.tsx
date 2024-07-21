16e110ad58d66880b658043c6183a1deaf6b916d
Yuxiang-Huang <yuxiangh142857@gmail.com>
2024-07-21 17:53:29 -0400

    Included the status in eatery card

--- src/components/info-card/eaterycard.tsx
+++ src/components/info-card/eaterycard.tsx
@@ -1,8 +1,11 @@
 import { Room } from '@/types';
-import { getEatingData, getImageURL } from '@/util/data/idToNames';
+import { getEatingData } from '@/util/data/idToNames';
 import React, { useEffect, useState } from 'react';
 import Image from 'next/image';
-import { IReadOnlyExtendedLocation } from '@/util/cmueats/types/locationTypes';
+import {
+  IReadOnlyExtendedLocation,
+  LocationState,
+} from '@/util/cmueats/types/locationTypes';
 import ButtonsRow from './ButtonsRow';
 import { ImSpoonKnife } from 'react-icons/im';
 
@@ -42,11 +45,55 @@ const Eaterycard = ({ room }: Props) => {
 
   const renderInfo = () => {
     if (eatingData) {
+      const colors: Record<LocationState, string> = {
+        [LocationState.OPEN]: '#19b875',
+        [LocationState.CLOSED]: '#dd3c18',
+        [LocationState.CLOSED_LONG_TERM]: '#dd3c18',
+        [LocationState.OPENS_SOON]: '#f6cc5d',
+        [LocationState.CLOSES_SOON]: '#f3f65d',
+      };
+
+      const getLocationState = () => {
+        if (eatingData.closedLongTerm) {
+          return LocationState.CLOSED_LONG_TERM;
+        }
+
+        const isOpen =
+          !eatingData.closedLongTerm &&
+          eatingData.statusMsg.indexOf('Closes') != -1;
+
+        if (isOpen) {
+          if (eatingData.changesSoon) {
+            return LocationState.CLOSES_SOON;
+          } else {
+            return LocationState.OPEN;
+          }
+        } else {
+          if (eatingData.changesSoon) {
+            return LocationState.OPENS_SOON;
+          } else {
+            return LocationState.CLOSED;
+          }
+        }
+      };
+
+      const color = colors[getLocationState()];
+
+      console.log(`bg-[${color}]`);
+
       return (
         <>
-          <div className="ml-3 mt-2">
+          <div className="mx-3 mt-2">
             <p className="font-bold">{eatingData.name}</p>
-            <p>{eatingData.statusMsg}</p>
+            <div className="flex items-center justify-between">
+              <p className={`text-[${color}]`}>{eatingData.statusMsg}</p>
+              <div
+                className={
+                  `h-3 w-3 rounded-full bg-[${color}] ` +
+                  (eatingData.changesSoon ? 'animate-blinking' : 'opacity-100')
+                }
+              ></div>
+            </div>
           </div>
           <div className="ml-3 mt-2 text-sm">{eatingData.shortDescription}</div>
         </>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,7 +7,19 @@ const config: Config = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        blinking: {
+          '0%': { opacity: '0' },
+          '50%': { opacity: '1' },
+          '75%': { opacity: '1' },
+          '100%': { opacity: '0' },
+        },
+      },
+      animation: {
+        blinking: 'blinking 1s infinite',
+      },
+    },
   },
   plugins: [],
 };
