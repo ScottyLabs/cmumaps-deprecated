cbb7d3606bb77711aab4bf4dcf0113f06507d3c8
Yuxiang-Huang <yuxiangh142857@gmail.com>
2024-07-28 23:18:08 -0400

    Moved EateryInfo into its own component

--- src/components/infocard/eaterycard.tsx
+++ src/components/infocard/eaterycard.tsx
@@ -1,12 +1,10 @@
 import { Room } from '@/types';
-import React, { useEffect, useState } from 'react';
 import Image from 'next/image';
-import {
-  IReadOnlyExtendedLocation,
-  LocationState,
-} from '@/util/cmueats/types/locationTypes';
 import ButtonsRow from './ButtonsRow';
 import { ImSpoonKnife } from 'react-icons/im';
+import EateryInfo from './EateryInfo';
+import React, { useEffect, useState } from 'react';
+import { IReadOnlyExtendedLocation } from '@/util/cmueats/types/locationTypes';
 import { getEatingData } from '@/util/cmueats/getEatingData';
 
 interface Props {
@@ -15,7 +13,7 @@ interface Props {
 
 const Eaterycard = ({ room }: Props) => {
   const [eatingData, setEatingData] =
-    useState<IReadOnlyExtendedLocation | null>();
+    useState<IReadOnlyExtendedLocation | null>(null);
 
   useEffect(() => {
     const fetchEatingData = async () => {
@@ -47,81 +45,6 @@ const Eaterycard = ({ room }: Props) => {
     }
   };
 
-  const renderInfo = () => {
-    if (eatingData) {
-      const getLocationState = () => {
-        if (eatingData.closedLongTerm) {
-          return LocationState.CLOSED_LONG_TERM;
-        }
-
-        const isOpen =
-          !eatingData.closedLongTerm &&
-          eatingData.statusMsg.indexOf('Closes') != -1;
-
-        if (isOpen) {
-          if (eatingData.changesSoon) {
-            return LocationState.CLOSES_SOON;
-          } else {
-            return LocationState.OPEN;
-          }
-        } else {
-          if (eatingData.changesSoon) {
-            return LocationState.OPENS_SOON;
-          } else {
-            return LocationState.CLOSED;
-          }
-        }
-      };
-
-      const locationState = getLocationState();
-
-      // reason for two dictionaries: https://tailwindcss.com/docs/content-configuration#class-detection-in-depth
-      // "Don’t construct class names dynamically"
-      const textColors: Record<LocationState, string> = {
-        [LocationState.OPEN]: 'text-[#19b875]',
-        [LocationState.CLOSED]: 'text-[#dd3c18]',
-        [LocationState.CLOSED_LONG_TERM]: 'text-[#dd3c18]',
-        [LocationState.OPENS_SOON]: 'text-[#f6cc5d]',
-        [LocationState.CLOSES_SOON]: 'text-[#f3f65d]',
-      };
-
-      const bgColors: Record<LocationState, string> = {
-        [LocationState.OPEN]: 'bg-[#19b875]',
-        [LocationState.CLOSED]: 'bg-[#dd3c18]',
-        [LocationState.CLOSED_LONG_TERM]: 'bg-[#dd3c18]',
-        [LocationState.OPENS_SOON]: 'bg-[#f6cc5d]',
-        [LocationState.CLOSES_SOON]: 'bg-[#f3f65d]',
-      };
-
-      const textColor = textColors[locationState];
-      const bgColor = bgColors[locationState];
-
-      const changesSoon =
-        locationState == LocationState.CLOSES_SOON ||
-        locationState == LocationState.OPENS_SOON;
-
-      return (
-        <>
-          <div className="mx-3 mt-2">
-            <p className="font-bold">{eatingData.name}</p>
-            <div className="flex items-center justify-between">
-              <p className={textColor}>{eatingData.statusMsg}</p>
-              <div
-                className={
-                  `${bgColor} h-3 w-3 rounded-full ` +
-                  (changesSoon ? 'animate-blinking' : 'opacity-100')
-                }
-              ></div>
-            </div>
-          </div>
-          <div className="ml-3 mt-2 text-sm">{eatingData.shortDescription}</div>
-        </>
-      );
-    } else {
-      return <div className="ml-3 mt-2 font-bold"> {room.alias}</div>;
-    }
-  };
-
   const renderButtonsRow = () => {
     const renderMiddleButton = () => {
       if (!eatingData) {
@@ -147,7 +70,7 @@ const Eaterycard = ({ room }: Props) => {
   return (
     <div>
       {renderEateryImage()}
-      {renderInfo()}
+      <EateryInfo room={room} eatingData={eatingData} />
       {renderButtonsRow()}
     </div>
   );
