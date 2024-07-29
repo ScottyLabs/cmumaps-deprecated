c462d994be650950f6c9fdf6f49dc14d86f38695
Yuxiang-Huang <yuxiangh142857@gmail.com>
2024-07-28 23:18:08 -0400

    Moved EateryInfo into its own component

--- src/components/infocard/BuildingCard.tsx
+++ src/components/infocard/BuildingCard.tsx
@@ -1,16 +1,19 @@
 import React from 'react';
-import Carousel from 'react-multi-carousel';
+// import Carousel from 'react-multi-carousel';
 import 'react-multi-carousel/lib/styles.css';
 import { HiMagnifyingGlass } from 'react-icons/hi2';
 import Image from 'next/image';
 import ButtonsRow from './ButtonsRow';
 import { Building } from '@/types';
+import { useAppSelector } from '@/lib/hooks';
 
 interface Props {
   building: Building;
 }
 
 const BuildingCard = ({ building }: Props) => {
+  const floorMap = useAppSelector((state) => state.data.floorMap);
+
   const renderBuildingImage = () => {
     const url = `/assets/location_images/building_room_images/${building.code}/${building.code}.jpg`;
 
@@ -40,73 +43,92 @@ const BuildingCard = ({ building }: Props) => {
     return <ButtonsRow middleButton={renderMiddleButton()} />;
   };
 
+  // const renderEateryCarousel = () => {
+  //   const renderEateryCard = (name: string) => {
+  //     return (
+  //       <div className="h-28 w-[calc(100vw-64px)] rounded-xl border border-[#dddddd] bg-white p-3">
+  //         {name}
+  //       </div>
+  //     );
+  //   };
+
+  //   const responsive = {
+  //     superLargeDesktop: {
+  //       breakpoint: { max: 4000, min: 3000 },
+  //       items: 5,
+  //     },
+  //     desktop: {
+  //       breakpoint: { max: 3000, min: 1024 },
+  //       items: 3,
+  //     },
+  //     tablet: {
+  //       breakpoint: { max: 1024, min: 464 },
+  //       items: 2,
+  //     },
+  //     mobile: {
+  //       breakpoint: { max: 464, min: 0 },
+  //       items: 1,
+  //       partialVisibilityGutter: 30,
+  //     },
+  //   };
+
+  //   // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  //   const CustomDot = ({ onClick, active }: any) => {
+  //     return (
+  //       <button
+  //         className={
+  //           'h-2 w-2 rounded-full ' +
+  //           `${active ? 'bg-[#8e8e8e]' : 'bg-[#f1f1f1]'}`
+  //         }
+  //         onClick={() => onClick()}
+  //       ></button>
+  //     );
+  //   };
+
+  //   return (
+  //     <div className="flex-column flex gap-2.5 pl-4">
+  //       <p className="my-0 font-medium">Eateries nearby</p>
+  //       <Carousel
+  //         responsive={responsive}
+  //         partialVisible
+  //         showDots
+  //         removeArrowOnDeviceType={['tablet', 'mobile']}
+  //         containerClass="!items-start h-[8.5rem]"
+  //         sliderClass=""
+  //         dotListClass="flex flex-row gap-2"
+  //         customDot={<CustomDot />}
+  //       >
+  //         <div className="carousel-item active">
+  //           {renderEateryCard('Eatery 1')}
+  //         </div>
+  //         <div className="carousel-item active">
+  //           {renderEateryCard('Eatery 2')}
+  //         </div>
+  //         <div className="carousel-item active">
+  //           {renderEateryCard('Eatery 3')}
+  //         </div>
+  //       </Carousel>
+  //     </div>
+  //   );
+  // };
+
   const renderEateryCarousel = () => {
-    const renderEateryCard = (name: string) => {
-      return (
-        <div className="h-28 w-[calc(100vw-64px)] rounded-xl border border-[#dddddd] bg-white p-3">
-          {name}
-        </div>
-      );
+    const getEateries = () => {
+      if (floorMap) {
+        return building.floors
+          .map((floor) => {
+            const rooms = floorMap[`${building.code}-${floor.name}`].rooms;
+            return Object.values(rooms).filter((room) => room.type == 'dining');
+          })
+          .flat();
+      } else {
+        return [];
+      }
     };
 
-    const responsive = {
-      superLargeDesktop: {
-        breakpoint: { max: 4000, min: 3000 },
-        items: 5,
-      },
-      desktop: {
-        breakpoint: { max: 3000, min: 1024 },
-        items: 3,
-      },
-      tablet: {
-        breakpoint: { max: 1024, min: 464 },
-        items: 2,
-      },
-      mobile: {
-        breakpoint: { max: 464, min: 0 },
-        items: 1,
-        partialVisibilityGutter: 30,
-      },
-    };
-
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    const CustomDot = ({ onClick, active }: any) => {
-      return (
-        <button
-          className={
-            'h-2 w-2 rounded-full ' +
-            `${active ? 'bg-[#8e8e8e]' : 'bg-[#f1f1f1]'}`
-          }
-          onClick={() => onClick()}
-        ></button>
-      );
-    };
-
-    return (
-      <div className="flex-column flex gap-2.5 pl-4">
-        <p className="my-0 font-medium">Eateries nearby</p>
-        <Carousel
-          responsive={responsive}
-          partialVisible
-          showDots
-          removeArrowOnDeviceType={['tablet', 'mobile']}
-          containerClass="!items-start h-[8.5rem]"
-          sliderClass=""
-          dotListClass="flex flex-row gap-2"
-          customDot={<CustomDot />}
-        >
-          <div className="carousel-item active">
-            {renderEateryCard('Eatery 1')}
-          </div>
-          <div className="carousel-item active">
-            {renderEateryCard('Eatery 2')}
-          </div>
-          <div className="carousel-item active">
-            {renderEateryCard('Eatery 3')}
-          </div>
-        </Carousel>
-      </div>
-    );
+    return getEateries().map((eatery) => (
+      <div key={eatery.id}>{eatery.name}</div>
+    ));
   };
 
   return (
---
+++ src/components/infocard/EateryInfo.tsx
@@ -0,0 +1,92 @@
+import { Room } from '@/types';
+import React from 'react';
+import {
+  IReadOnlyExtendedLocation,
+  LocationState,
+} from '@/util/cmueats/types/locationTypes';
+
+interface Props {
+  room: Room;
+  eatingData: IReadOnlyExtendedLocation | null;
+}
+
+const EateryInfo = ({ room, eatingData }: Props) => {
+  if (!eatingData) {
+    return <div className="ml-3 mt-2 font-bold"> {room.alias}</div>;
+  }
+
+  const getLocationState = () => {
+    if (eatingData.closedLongTerm) {
+      return LocationState.CLOSED_LONG_TERM;
+    }
+
+    const isOpen =
+      !eatingData.closedLongTerm &&
+      eatingData.statusMsg.indexOf('Closes') != -1;
+
+    if (isOpen) {
+      if (eatingData.changesSoon) {
+        return LocationState.CLOSES_SOON;
+      } else {
+        return LocationState.OPEN;
+      }
+    } else {
+      if (eatingData.changesSoon) {
+        return LocationState.OPENS_SOON;
+      } else {
+        return LocationState.CLOSED;
+      }
+    }
+  };
+
+  const locationState = getLocationState();
+
+  // reason for two dictionaries: https://tailwindcss.com/docs/content-configuration#class-detection-in-depth
+  // "Don’t construct class names dynamically"
+  const textColors: Record<LocationState, string> = {
+    [LocationState.OPEN]: 'text-[#19b875]',
+    [LocationState.CLOSED]: 'text-[#dd3c18]',
+    [LocationState.CLOSED_LONG_TERM]: 'text-[#dd3c18]',
+    [LocationState.OPENS_SOON]: 'text-[#f6cc5d]',
+    [LocationState.CLOSES_SOON]: 'text-[#f3f65d]',
+  };
+
+  const bgColors: Record<LocationState, string> = {
+    [LocationState.OPEN]: 'bg-[#19b875]',
+    [LocationState.CLOSED]: 'bg-[#dd3c18]',
+    [LocationState.CLOSED_LONG_TERM]: 'bg-[#dd3c18]',
+    [LocationState.OPENS_SOON]: 'bg-[#f6cc5d]',
+    [LocationState.CLOSES_SOON]: 'bg-[#f3f65d]',
+  };
+
+  const textColor = textColors[locationState];
+  const bgColor = bgColors[locationState];
+
+  const changesSoon =
+    locationState == LocationState.CLOSES_SOON ||
+    locationState == LocationState.OPENS_SOON;
+
+  const renderStatusCircle = () => (
+    <div
+      className={
+        `${bgColor} h-3 w-3 rounded-full ` +
+        (changesSoon ? 'animate-blinking' : 'opacity-100')
+      }
+    />
+  );
+
+  return (
+    <>
+      <div className="mx-3 mt-2">
+        <p className="font-bold">{eatingData.name}</p>
+        <div className="flex items-center justify-between">
+          <p className={textColor}>{eatingData.statusMsg}</p>
+        </div>
+        {renderStatusCircle()}
+      </div>
+      <div className="ml-3 mt-2 text-sm">{eatingData.shortDescription}</div>
+    </>
+  );
+};
+
+export default EateryInfo;
