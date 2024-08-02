ab18a246a7cbde2207bc6e93c7db343437ae57fc
Yuxiang-Huang <yuxiangh142857@gmail.com>
2024-08-02 00:03:58 -0400

    Rework the events api for getting room schedule

---
+++ src/app/api/getRoomSchedule/route.ts
@@ -0,0 +1,43 @@
+import prisma from '@/lib/prisma';
+import { NextRequest, NextResponse } from 'next/server';
+
+export async function POST(request: NextRequest) {
+  try {
+    const requestData = await request.json();
+    const roomName = requestData.roomName;
+
+    console.log(roomName);
+
+    console.log('here?');
+
+    const events = await prisma.event.findMany({
+      orderBy: [{ startTime: 'asc' }],
+      where: {
+        roomName,
+        // date: {
+        //   lte: endDate,
+        //   gte: startDate,
+        // },
+      },
+    });
+
+    console.log(events);
+
+    // good response
+    return new NextResponse(JSON.stringify({ events }), {
+      status: 200,
+    });
+  } catch (e) {
+    // Javascript Error Message
+    // console.log(e);
+    return new NextResponse(
+      JSON.stringify({
+        error: String(e),
+        // error: String(e.stack),
+      }),
+      {
+        status: 500,
+      },
+    );
+  }
+}
