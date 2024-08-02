ecddaba951d1bd2a879303503e5a8f43d7a71e90
Yuxiang-Huang <yuxiangh142857@gmail.com>
2024-08-02 16:16:14 -0400

    Rough draft of the eatery carousel done

--- src/components/infocard/BuildingCard.tsx
+++ src/components/infocard/BuildingCard.tsx
@@ -1,8 +1,9 @@
 import Image from 'next/image';
 
 import React, { useEffect, useState } from 'react';
+import { isDesktop } from 'react-device-detect';
 import { HiMagnifyingGlass } from 'react-icons/hi2';
-// import Carousel from 'react-multi-carousel';
+import Carousel from 'react-multi-carousel';
 import 'react-multi-carousel/lib/styles.css';
 
 import { claimRoom } from '@/lib/features/uiSlice';
@@ -94,89 +95,80 @@ const BuildingCard = ({ building }: Props) => {
     return <ButtonsRow middleButton={renderMiddleButton()} />;
   };
 
-  // const renderEateryCarousel = () => {
-  //   const renderEateryCard = (name: string) => {
-  //     return (
-  //       <div className="h-28 w-[calc(100vw-64px)] rounded-xl border border-[#dddddd] bg-white p-3">
-  //         {name}
-  //       </div>
-  //     );
-  //   };
-
-  //   const responsive = {
-  //     superLargeDesktop: {
-  //       breakpoint: { max: 4000, min: 3000 },
-  //       items: 5,
-  //     },
-  //     desktop: {
-  //       breakpoint: { max: 3000, min: 1024 },
-  //       items: 3,
-  //     },
-  //     tablet: {
-  //       breakpoint: { max: 1024, min: 464 },
-  //       items: 2,
-  //     },
-  //     mobile: {
-  //       breakpoint: { max: 464, min: 0 },
-  //       items: 1,
-  //       partialVisibilityGutter: 30,
-  //     },
-  //   };
-
-  //   // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  //   const CustomDot = ({ onClick, active }: any) => {
-  //     return (
-  //       <button
-  //         className={
-  //           'h-2 w-2 rounded-full ' +
-  //           `${active ? 'bg-[#8e8e8e]' : 'bg-[#f1f1f1]'}`
-  //         }
-  //         onClick={() => onClick()}
-  //       ></button>
-  //     );
-  //   };
-
-  //   return (
-  //     <div className="flex-column flex gap-2.5 pl-4">
-  //       <p className="my-0 font-medium">Eateries nearby</p>
-  //       <Carousel
-  //         responsive={responsive}
-  //         partialVisible
-  //         showDots
-  //         removeArrowOnDeviceType={['tablet', 'mobile']}
-  //         containerClass="!items-start h-[8.5rem]"
-  //         sliderClass=""
-  //         dotListClass="flex flex-row gap-2"
-  //         customDot={<CustomDot />}
-  //       >
-  //         <div className="carousel-item active">
-  //           {renderEateryCard('Eatery 1')}
-  //         </div>
-  //         <div className="carousel-item active">
-  //           {renderEateryCard('Eatery 2')}
-  //         </div>
-  //         <div className="carousel-item active">
-  //           {renderEateryCard('Eatery 3')}
-  //         </div>
-  //       </Carousel>
-  //     </div>
-  //   );
-  // };
-
   const renderEateryCarousel = () => {
-    return (
-      <div className="mx-2 mb-3 space-y-3">
-        {eatingData.map(([eatery, eatingData]) => (
-          <div
-            key={eatery.id}
-            className="cursor-pointer rounded border p-1"
-            onClick={() => dispatch(claimRoom(eatery))}
+    if (isDesktop) {
+      return (
+        <div className="mx-2 mb-3 space-y-3">
+          {eatingData.map(([eatery, eatingData]) => (
+            <div
+              key={eatery.id}
+              className="cursor-pointer rounded border p-1"
+              onClick={() => dispatch(claimRoom(eatery))}
+            >
+              <EateryInfo room={eatery} eatingData={eatingData} />
+            </div>
+          ))}
+        </div>
+      );
+    } else {
+      const responsive = {
+        superLargeDesktop: {
+          breakpoint: { max: 4000, min: 3000 },
+          items: 5,
+        },
+        desktop: {
+          breakpoint: { max: 3000, min: 1024 },
+          items: 3,
+        },
+        tablet: {
+          breakpoint: { max: 1024, min: 464 },
+          items: 2,
+        },
+        mobile: {
+          breakpoint: { max: 464, min: 0 },
+          items: 1,
+          partialVisibilityGutter: 30,
+        },
+      };
+
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      const CustomDot = ({ onClick, active }: any) => {
+        return (
+          <button
+            className={
+              'h-2 w-2 rounded-full ' +
+              `${active ? 'bg-[#8e8e8e]' : 'bg-[#f1f1f1]'}`
+            }
+            onClick={() => onClick()}
+          ></button>
+        );
+      };
+
+      return (
+        <div className="ml-2 mb-1">
+          <p className="my-0 font-medium">Eateries nearby</p>
+          <Carousel
+            responsive={responsive}
+            showDots
+            arrows={false}
+            dotListClass="gap-2"
+            customDot={<CustomDot />}
           >
-            <EateryInfo room={eatery} eatingData={eatingData} />
-          </div>
-        ))}
-      </div>
-    );
+            {eatingData.map(([eatery, eatingData]) => (
+              <div
+                key={eatery.id}
+                className="cursor-pointer rounded border p-1"
+                onClick={() => dispatch(claimRoom(eatery))}
+              >
+                <div className="carousel-item active">
+                  <EateryInfo room={eatery} eatingData={eatingData} />
+                </div>
+              </div>
+            ))}
+          </Carousel>
+        </div>
+      );
+    }
   };
 
   return (
