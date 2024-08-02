43fcb553c1d6341b1359af4f948b8d7fb69498eb
Yuxiang-Huang <yuxiangh142857@gmail.com>
2024-08-02 00:27:36 -0400

    Gave up on events for now

--- src/app/api/getRoomSchedule/route.ts
+++ src/app/api/getRoomSchedule/route.ts
@@ -6,20 +6,20 @@ export async function POST(request: NextRequest) {
     const requestData = await request.json();
     const roomName = requestData.roomName;
 
-    console.log(roomName);
-
-    console.log('here?');
+    console.log(await prisma.room.findFirst());
 
     const events = await prisma.event.findMany({
       orderBy: [{ startTime: 'asc' }],
       where: {
-        roomName,
+        roomName: 'TEP 2612',
         // date: {
         //   lte: endDate,
         //   gte: startDate,
         // },
       },
     });
+
+    console.log(events);
 
     // good response
     return new NextResponse(JSON.stringify({ events }), {
