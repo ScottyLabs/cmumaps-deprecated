2c1f26bd4bd65324840c28f1ed79de507672ec88
Yuxiang-Huang <yuxiangh142857@gmail.com>
2024-08-04 01:50:29 -0400

    Able to load all the polygons of the rooms

--- src/components/buildings/FloorPlanOverlay.tsx
+++ src/components/buildings/FloorPlanOverlay.tsx
@@ -1,6 +1,7 @@
+import { Position } from 'geojson';
 import { Annotation, Coordinate, Polygon } from 'mapkit-react';
 
-import React, { useEffect, useMemo, useState } from 'react';
+import React, { useCallback, useEffect, useMemo, useState } from 'react';
 
 import { getFloorPlan } from '@/lib/apiRoutes';
 import { claimRoom, releaseRoom } from '@/lib/features/uiSlice';
@@ -18,14 +19,11 @@ import { latitudeRatio, longitudeRatio, rotate } from '@/util/geometry';
 import styles from '../../styles/FloorPlanOverlay.module.css';
 import RoomPin, { hasIcon } from './RoomPin';
 
-export function getFloorCenter(rooms: Room[]): AbsoluteCoordinate | undefined {
-  if (!rooms) {
-    return undefined;
-  }
-
-  let points: AbsoluteCoordinate[] = Object.values(rooms).flatMap(
-    (room: Room) => room?.polygon?.coordinates.flat(),
+export const getFloorCenter = (rooms: Room[]): Position => {
+  let points: Position[] = Object.values(rooms).flatMap((room: Room) =>
+    room.polygon.coordinates.flat(),
   );
+
   points = points.filter((e) => e !== undefined);
 
   const allX = points.map((p) => p[0]);
@@ -37,19 +35,13 @@ export function getFloorCenter(rooms: Room[]): AbsoluteCoordinate | undefined {
   const maxY = Math.max(...allY);
 
   return [(minX + maxX) / 2, (minY + maxY) / 2];
-}
+};
 
 export function positionOnMap(
-  absolute: AbsoluteCoordinate,
-  placement: Placement | null,
-  center: AbsoluteCoordinate | undefined,
+  absolute: Position,
+  placement: Placement,
+  center: Position,
 ) {
-  if (placement === null) {
-    throw new Error('No active placement');
-  }
-  if (!center) {
-    throw new Error('No center');
-  }
   const [absoluteY, absoluteX] = rotate(
     absolute[0] - center[0],
     absolute[1] - center[1],
@@ -75,6 +67,9 @@ export default function FloorPlanOverlay({
   floor,
   showRoomNames,
 }: FloorPlanOverlayProps) {
+  const dispatch = useAppDispatch();
+  const selectedRoom = useAppSelector((state) => state.ui.selectedRoom);
+
   const [floorPlan, setFloorPlan] = useState<FloorPlan | null>(null);
 
   useEffect(() => {
@@ -83,111 +78,93 @@ export default function FloorPlanOverlay({
     });
   }, [floor]);
 
-  if (floorPlan) {
-    console.log(floorPlan);
+  if (!floorPlan) {
+    return;
   }
 
   const { placement, rooms } = floorPlan;
 
   // Compute the center position of the bounding box of the current floor
   // (Will be used as the rotation center)
-  const center: AbsoluteCoordinate | undefined = useMemo(
-    () => getFloorCenter(rooms),
-    [rooms],
-  );
-  const convertToMap = (absolute: AbsoluteCoordinate): Coordinate =>
+  const center = getFloorCenter(rooms);
+
+  const convertToMap = (absolute: Position): Coordinate =>
     positionOnMap(absolute, placement, center);
-  const dispatch = useAppDispatch();
-  const selectedRoom = useAppSelector((state) => state.ui.selectedRoom);
 
-  return (
-    <>
-      {Object.entries(rooms).map((entry) => {
-        const id: string = entry[0];
-        const room: Room = { ...entry[1], id };
+  return Object.entries(rooms).map(([roomId, room]) => {
+    const pointsSrcAbs = room?.polygon?.coordinates.map((shape) =>
+      shape.filter((e) => !!e),
+    );
 
-        // Turn on for CMUShits.com
-        // if (room.type != "restroom" && room.type != "corridor")
-        //   return;
+    const pointsSrc = room.polygon.coordinates.map((shape) =>
+      shape.map(convertToMap),
+    );
 
-        const pointsSrcAbs = room?.polygon?.coordinates.map((shape) =>
-          shape.filter((e) => !!e),
-        );
+    const roomColors = getRoomTypeDetails(room.type);
 
-        if (!pointsSrcAbs) {
-          return;
-        }
-        room.labelPosition = [room.labelPosition?.x, room.labelPosition?.y];
-        const roomCenter = room.labelPosition || [
-          pointsSrcAbs[0].reduce((a, b) => a + b[0], 0) /
-            pointsSrcAbs[0].length,
-          pointsSrcAbs[0].reduce((a, b) => a + b[1], 0) /
-            pointsSrcAbs[0].length,
-        ];
+    const roomCenter = room.labelPosition || [
+      pointsSrcAbs[0].reduce((a, b) => a + b[0], 0) / pointsSrcAbs[0].length,
+      pointsSrcAbs[0].reduce((a, b) => a + b[1], 0) / pointsSrcAbs[0].length,
+    ];
 
-        const pointsSrc = pointsSrcAbs.map((shape) => shape.map(convertToMap));
+    // const labelPos = convertToMap(roomCenter);
 
-        const roomColors = getRoomTypeDetails(room.type);
+    // const opacity = isBackground ? 0.7 : 1;
+    const opacity = 1;
 
-        const labelPos = convertToMap(roomCenter);
+    const showIcon = hasIcon(room) || selectedRoom?.id === roomId;
 
-        const opacity = isBackground ? 0.7 : 1;
+    const gutter = selectedRoom?.id === roomId ? 20 : 4;
+    const iconSize = selectedRoom?.id === roomId ? 20 : showIcon ? 20 : 10;
+    const labelHeight = 24;
+    const labelOffset = {
+      left: iconSize + gutter,
+      top: (iconSize - labelHeight) / 2,
+    };
 
-        const showIcon = hasIcon(room) || selectedRoom?.id === id;
+    return (
+      <React.Fragment key={room.name}>
+        <Polygon
+          points={[...pointsSrc]}
+          selected={selectedRoom?.id === roomId}
+          enabled={true}
+          fillColor={roomColors.background}
+          fillOpacity={opacity}
+          strokeColor={
+            selectedRoom?.id === roomId ? '#f7efc3' : roomColors.border
+          }
+          strokeOpacity={opacity}
+          lineWidth={selectedRoom?.id === roomId ? 5 : 1}
+          onSelect={() => dispatch(claimRoom(room))}
+          onDeselect={() => dispatch(releaseRoom(room))}
+          fillRule="nonzero"
+        />
 
-        const gutter = selectedRoom?.id === id ? 20 : 4;
-        const iconSize = selectedRoom?.id === id ? 20 : showIcon ? 20 : 10;
-        const labelHeight = 24;
-        const labelOffset = {
-          left: iconSize + gutter,
-          top: (iconSize - labelHeight) / 2,
-        };
-
-        return (
-          <React.Fragment key={room.name}>
-            <Polygon
-              points={[...pointsSrc]}
-              selected={selectedRoom?.id === id}
-              enabled={true}
-              fillColor={roomColors.background}
-              fillOpacity={opacity}
-              strokeColor={
-                selectedRoom?.id === id ? '#f7efc3' : roomColors.border
-              }
-              strokeOpacity={opacity}
-              lineWidth={selectedRoom?.id === id ? 5 : 1}
-              onSelect={() => dispatch(claimRoom(room))}
-              onDeselect={() => dispatch(releaseRoom(room))}
-              fillRule="nonzero"
-            />
-
-            {!isBackground && (showRoomNames || showIcon) && (
-              <Annotation
-                latitude={labelPos.latitude}
-                longitude={labelPos.longitude}
-                onSelect={() => dispatch(claimRoom(room))}
-                onDeselect={() => dispatch(releaseRoom(room))}
-              >
+        {/* {!isBackground && (showRoomNames || showIcon) && (
+          <Annotation
+            latitude={labelPos.latitude}
+            longitude={labelPos.longitude}
+            onSelect={() => dispatch(claimRoom(room))}
+            onDeselect={() => dispatch(releaseRoom(room))}
+          >
+            <div
+              className={`relative width-[${iconSize}] height-[${iconSize}]`}
+            >
+              <RoomPin room={room} />
+              {(showRoomNames || room.alias) && (
                 <div
-                  className={`relative width-[${iconSize}] height-[${iconSize}]`}
+                  className={`flex-1 flex-col justify-center height-[${labelHeight}] absolute left-[${labelOffset.left}] top-[${labelOffset.top}] text-sm leading-[1.1] tracking-wide`}
                 >
-                  <RoomPin room={room} />
-                  {(showRoomNames || room.alias) && (
-                    <div
-                      className={`flex-1 flex-col justify-center height-[${labelHeight}] absolute left-[${labelOffset.left}] top-[${labelOffset.top}] text-sm leading-[1.1] tracking-wide`}
-                    >
-                      {showRoomNames && (
-                        <div className={styles['room-number']}>{room.name}</div>
-                      )}
-                      {room.alias && <div>{room.alias}</div>}
-                    </div>
+                  {showRoomNames && (
+                    <div className={styles['room-number']}>{room.name}</div>
                   )}
+                  {room.alias && <div>{room.alias}</div>}
                 </div>
-              </Annotation>
-            )}
-          </React.Fragment>
-        );
-      })}
-    </>
-  );
+              )}
+            </div>
+          </Annotation>
+        )} */}
+      </React.Fragment>
+    );
+  });
 }
