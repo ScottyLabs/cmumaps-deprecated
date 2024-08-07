d70d830694815e3f1f23c1876f4a9c3f3757929c
Yuxiang-Huang <yuxiangh142857@gmail.com>
2024-08-06 23:53:01 -0400

    Clean up MapDisplay

--- src/components/buildings/MapDisplay.tsx
+++ src/components/buildings/MapDisplay.tsx
@@ -1,29 +1,27 @@
 import {
-  Coordinate,
   FeatureVisibility,
   Map,
   MapType,
   PointOfInterestCategory,
 } from 'mapkit-react';
 
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 
 import {
-  claimRoom,
   deselectBuilding,
   releaseRoom,
   setFocusedFloor,
   setIsSearchOpen,
 } from '@/lib/features/uiSlice';
 import { useAppDispatch, useAppSelector } from '@/lib/hooks';
-import { AbsoluteCoordinate, Building, BuildingCode, Room } from '@/types';
+import { Building } from '@/types';
 import { isInPolygonCoordinates } from '@/util/geometry';
 
 import useMapPosition from '../../hooks/useMapPosition';
 import NavLine from '../navigation/NavLine';
 import BuildingShape from './BuildingShape';
-import FloorPlanOverlay, { getFloorCenter } from './FloorPlanOverlay';
-import { getBuildingDefaultFloorToFocus, zoomOnObject } from './mapUtils';
+import FloorPlanOverlay from './FloorPlanOverlay';
+import { getBuildingDefaultFloorToFocus } from './mapUtils';
 
 interface MapDisplayProps {
   mapRef: React.RefObject<mapkit.Map | null>;
@@ -58,121 +56,6 @@ const MapDisplay = ({ mapRef }: MapDisplayProps) => {
 
   const [showFloor, setShowFloor] = useState<boolean>(false);
   const [showRoomNames, setShowRoomNames] = useState(false);
-
-  const showBuilding = (newBuilding: Building | null, updateMap: boolean) => {
-    // dispatch(focusBuilding(newBuilding));
-    if (newBuilding === null) {
-      return;
-    }
-
-    if (updateMap) {
-      // Updates viewbox based on OSM shape
-      const points: Coordinate[] = newBuilding.shapes.flat();
-
-      zoomOnObject(points);
-
-      setShowFloor(true);
-      setShowRoomNames(false);
-    }
-    // If no floor set, find default floor and set it to that
-    if (!updateMap && !focusedFloor && newBuilding.floors.length > 0) {
-      const floorLevel = newBuilding.defaultFloor;
-      dispatch(
-        setFocusedFloor({
-          buildingCode: newBuilding.code,
-          level: floorLevel,
-        }),
-      );
-    }
-  };
-
-  const showRoom = (
-    newRoom: Room,
-    newBuilding: Building | null,
-    convertToMap: (absolute: AbsoluteCoordinate) => Coordinate,
-    updateMap: boolean,
-  ) => {
-    if (newBuilding === null) {
-      return;
-    }
-
-    if (updateMap) {
-      const points: AbsoluteCoordinate[] = newRoom.polygon.coordinates.flat();
-      const coords: Coordinate[] = points.map((p) => convertToMap(p));
-      zoomOnObject(coords);
-
-      setShowFloor(true);
-      setShowRoomNames(true);
-    }
-  };
-
-  const zoomOnDefaultBuilding = (
-    newBuildings: Record<BuildingCode, Building>,
-    newFloors: FloorMap | null,
-  ) => {
-    // Gets from URL and sets the default building and floor and room
-
-    // Make sure that both buildings and the map are loaded
-    if (!newBuildings || !mapRef.current) {
-      return;
-    }
-
-    // Get defaults from URL
-    const [_, floor, roomid] = (window?.location?.pathname || '').split('/');
-    const r = new RegExp('-|#');
-    let [buildingCode, floorLevel]: [BuildingCode, any] = floor
-      .toUpperCase()
-      .split(r);
-    console.log(
-      'searchme',
-      window?.location?.pathname,
-      buildingCode,
-      floorLevel,
-      roomid,
-    );
-    // If building is not found, do nothing
-    const building: Building | undefined = newBuildings[buildingCode];
-
-    if (!building) {
-      window.history.pushState({}, '', window.location.origin);
-      return;
-    }
-
-    // If floor is not specified, set it to the default floor
-    if (!floorLevel) {
-      floorLevel = building.defaultFloor.split('-')[1];
-    }
-
-    console.log('searchme1', floorLevel);
-    dispatch(
-      setFocusedFloor({ buildingCode: building.code, level: floorLevel }),
-    );
-    // dispatch(focusBuilding(building));
-
-    if (newFloors) {
-      const floor = building.floors.find(({ name }) => name == floorLevel)!; // .find({ level } => level == floorLevel)! once replace file format
-      const floorPlan = newFloors[`${building.code}-${floor.name}`];
-
-      const { rooms, placement } = floorPlan;
-      // Compute the center position of the bounding box of the current floor
-      // (Will be used as the rotation center)
-      const center: AbsoluteCoordinate | undefined = getFloorCenter(rooms);
-      const convertToMap = (absolute: AbsoluteCoordinate): Coordinate =>
-        positionOnMap(absolute, placement, center);
-
-      // If we also have the room in the URL, focus on that
-      if (floorPlan && roomid) {
-        const room = floorPlan.rooms.find((room: Room) => room.id === roomid);
-        room && dispatch(claimRoom(room));
-        room && showRoom(room, building, convertToMap, true);
-      } else {
-        // Otherwise, zoom on the just the building
-        showBuilding(building, true);
-      }
-    } else {
-      showBuilding(building, true);
-    }
-  };
 
   // React to pan/zoom events
   const { onRegionChangeStart, onRegionChangeEnd } = useMapPosition(
