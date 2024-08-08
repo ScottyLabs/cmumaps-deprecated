aa6248d50b960a82e6c318a96c547e3b2d643f2d
Yuxiang-Huang <yuxiangh142857@gmail.com>
2024-08-07 22:47:47 -0400

    Fixed WEH-3

--- src/components/buildings/MapDisplay.tsx
+++ src/components/buildings/MapDisplay.tsx
@@ -199,8 +199,6 @@ const MapDisplay = ({ mapRef }: MapDisplayProps) => {
     const ordinal =
       focusedBuilding.defaultOrdinal + focusedIndex - defaultIndex;
 
-    console.log(visibleBuildings);
-
     // get all the floor plans
     const promises = visibleBuildings.map(async (building) => {
       const floor = getFloorAtOrdinal(building, ordinal);
