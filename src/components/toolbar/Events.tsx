60b245a1f6241241f977d3baba707d43a714ff10
Yuxiang-Huang <yuxiangh142857@gmail.com>
2024-08-09 17:09:10 -0400

    Add simple mock up for orientation events

---
+++ src/components/toolbar/Events.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import CollapsibleWrapper from '../common/CollapsibleWrapper';
+
+const Events = () => {
+  return (
+    <CollapsibleWrapper title="Orientation Events">
+      <div className="px-5 pb-1">
+        <p>Let me know when the orientation events came out!</p>
+      </div>
+    </CollapsibleWrapper>
+  );
+};
+
+export default Events;
