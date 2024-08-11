d8fa983b4aeead16d0b79f430988771245ccbce0
Yuxiang-Huang <yuxiangh142857@gmail.com>
2024-08-11 03:23:28 -0400

    Finished get Eatery data ts

--- scripts/getEateryData.ts
+++ scripts/getEateryData.ts
@@ -4,8 +4,49 @@ import { EateryData } from '../src/types';
 // Promise<EateryData[]>
 
 export const getEateryData = async () => {
-  const res = await fetch('https://dining.apis.scottylabs.org/locations/');
-  const cmueatsData = (await res.json())['locations'];
+  //   const res = await fetch('https://dining.apis.scottylabs.org/locations/');
+  //   const cmueatsData = (await res.json())['locations'];
+
+  const cmueatsData = [
+    {
+      conceptId: 113,
+      name: 'AU BON PAIN AT SKIBO CAFÉ',
+      shortDescription:
+        'Coffee/tea, espresso, soup, sandwiches/salads, grab-n-go, yogurt parfaits, fruit, snacks',
+      description:
+        'At Au Bon Pain café bakery, each signature recipe is uniquely crafted. You can enjoy delicious hot or iced coffee and teas, espresso drinks, a variety of cold beverages, soup, a customized made-to-order breakfast or lunch sandwich or salad, or you can grab a pre-made salad, sandwich, wrap, yogurt parfait, fresh fruit or snack. There is always something new to try ... healthy choices, comfort food, indulgent treats … try them all!  Nutritional information can be found at aubonpain.com/nutrition. To place a catering order, please email abpcmu@grcafes.com. For on-campus assistance, call 804-839-6774.',
+      url: 'https://apps.studentaffairs.cmu.edu/dining/conceptinfo/Concept/113',
+      location: 'Cohon Center, Second floor',
+      coordinates: { lat: 40.444107, lng: -79.942206 },
+      acceptsOnlineOrders: true,
+      times: [
+        {
+          start: { day: 0, hour: 1, minute: 0 },
+          end: { day: 0, hour: 2, minute: 0 },
+        },
+        {
+          start: { day: 1, hour: 8, minute: 0 },
+          end: { day: 1, hour: 14, minute: 0 },
+        },
+        {
+          start: { day: 3, hour: 8, minute: 0 },
+          end: { day: 3, hour: 14, minute: 0 },
+        },
+        {
+          start: { day: 4, hour: 8, minute: 0 },
+          end: { day: 4, hour: 14, minute: 0 },
+        },
+        {
+          start: { day: 5, hour: 8, minute: 0 },
+          end: { day: 5, hour: 14, minute: 0 },
+        },
+        {
+          start: { day: 6, hour: 8, minute: 0 },
+          end: { day: 6, hour: 15, minute: 0 },
+        },
+      ],
+    },
+  ];
 
   const daysOfWeek = [
     'Sunday', // 0
@@ -45,15 +86,19 @@ export const getEateryData = async () => {
     return hoursDifference;
   };
 
-  const getNextOpenTime = (eatery, todayStartTime) => {
-    // today's session haven't started
-    if (now < todayStartTime) {
-      return todayStartTime;
-    }
+  const closeHelper = (nextDate: Date, ans: Partial<EateryData>) => {
+    const hourDif = getHourDif(now, nextDate);
 
-    // find next date
-    console.log('here');
-    console.log(eatery);
+    if (hourDif < 1) {
+      ans.locationState = 'OPENS_SOON';
+      ans.statusMsg = `Close (${Math.round(hourDif * 60)} minutes until open)`;
+    } else if (hourDif < 24) {
+      ans.locationState = 'CLOSED';
+      ans.statusMsg = `Open (${Math.round(hourDif)} hours until open)`;
+    } else {
+      ans.locationState = 'CLOSED';
+      ans.statusMsg = `Close (${Math.ceil(hourDif / 24)} days until open on ${daysOfWeek[nextDate.getDay()]})`;
+    }
   };
 
   const getStatusMsgAndLocationState = (
@@ -83,16 +128,23 @@ export const getEateryData = async () => {
       }
       // when close
       else {
-        const nextOpenTime = getNextOpenTime(eatery, startTime);
-        const hourDif = getHourDif(now, nextOpenTime);
+        const getNextDate = (eatery): Date => {
+          // today's session haven't started
+          if (now < startTime) {
+            return startTime;
+          }
 
-        if (hourDif > 1) {
-          ans.locationState = 'OPENS_SOON';
-          ans.statusMsg = `Open (${Math.round(hourDif)} hours until open)`;
-        } else {
-          ans.locationState = 'OPENS_SOON';
-          ans.statusMsg = `Close (${Math.round(hourDif * 60)} minutes until open)`;
-        }
+          const todayIndex = eatery.times.findIndex(
+            (time) => time.start.day == currentDay,
+          );
+
+          return convertToDate(
+            eatery.times[(todayIndex + 1) % eatery.times.length].start,
+          );
+        };
+
+        const nextDate = getNextDate(eatery);
+        closeHelper(nextDate, ans);
       }
     } else {
       const makeWrapAround = (times) => {
@@ -110,15 +162,7 @@ export const getEateryData = async () => {
 
       const nextDay = eatery.times.find((time) => time.start.day > currentDay);
       const nextDate = convertToDate(nextDay.start);
-      const hourDif = getHourDif(now, nextDate);
-
-      if (hourDif < 1) {
-        ans.locationState = 'OPENS_SOON';
-        ans.statusMsg = `Close (${Math.round(hourDif * 60)} minutes until open)`;
-      } else {
-        ans.locationState = 'CLOSED';
-        ans.statusMsg = `Close (${Math.ceil(hourDif / 24)} days until open on ${daysOfWeek[nextDate.getDay()]})`;
-      }
+      closeHelper(nextDate, ans);
     }
   };
 
@@ -127,8 +171,6 @@ export const getEateryData = async () => {
     ans.name = eatery.name;
     ans.url = eatery.url;
     ans.shortDescription = eatery.shortDescription;
-
-    console.log(JSON.stringify(eatery));
 
     if (eatery.times.length == 0) {
       ans.locationState = 'CLOSED_LONG_TERM';
@@ -142,15 +184,6 @@ export const getEateryData = async () => {
   console.log(
     cmueatsData.slice(0, 1).map((eatery: any) => parseEatery(eatery)),
   );
-
-  //   console.log(
-  //     cmueatsData.slice(20, 21).map((eatery: any) => parseEatery(eatery)),
-  //   );
-
-  //   console.log(
-  //     cmueatsData.slice(23, 24).map((eatery: any) => parseEatery(eatery)),
-  //   );
 };
 
 getEateryData();
-// console.log(await getEateryData());
