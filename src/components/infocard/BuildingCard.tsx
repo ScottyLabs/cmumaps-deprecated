0d1a9758f8cdf21d8989b10e83badbea8b1f36d4
Yuxiang-Huang <yuxiangh142857@gmail.com>
2024-08-11 14:24:40 -0400

    Also order the eateries in building card

--- src/components/infocard/BuildingCard.tsx
+++ src/components/infocard/BuildingCard.tsx
@@ -1,10 +1,11 @@
-import React, { useEffect, useState } from 'react';
-import Carousel from 'react-multi-carousel';
+import React from 'react';
+// import Carousel from 'react-multi-carousel';
 import 'react-multi-carousel/lib/styles.css';
 
 import { claimRoom } from '@/lib/features/uiSlice';
 import { useAppDispatch, useAppSelector } from '@/lib/hooks';
-import { Building, EateryInfo, SearchRoom } from '@/types';
+import { Building, SearchRoom } from '@/types';
+import { sortEateries } from '@/util/eateryUtils';
 
 import { zoomOnSearchRoom } from '../buildings/mapUtils';
 import ButtonsRow from './ButtonsRow';
@@ -26,44 +27,6 @@ const BuildingCard = ({ map, building }: Props) => {
   const eateryData = useAppSelector((state) => state.data.eateryData);
   const floorPlanMap = useAppSelector((state) => state.data.floorPlanMap);
 
-  const [eatingData, setEatingData] = useState<
-    [SearchRoom, EateryInfo | null][]
-  >([]);
-
-  // get eatery data
-  useEffect(() => {
-    const getEateries = () => {
-      return building.floors
-        .map((floorLevel) => {
-          // remove this later!!!
-          if (
-            !searchMap[`${building.code}`] ||
-            !searchMap[`${building.code}`][floorLevel]
-          ) {
-            return [];
-          }
-          const rooms = searchMap[`${building.code}`][`${floorLevel}`];
-          return rooms.filter((room) => room.type == 'food');
-        })
-        .flat();
-    };
-
-    const eateries = getEateries();
-
-    const newEatingData: [SearchRoom, EateryInfo | null][] = eateries.map(
-      (eatery) => {
-        if (eatery.aliases[0]) {
-          const data = eateryData[eatery.aliases[0].toUpperCase()];
-          return [eatery, data];
-        } else {
-          return [eatery, null];
-        }
-      },
-    );
-
-    setEatingData(newEatingData);
-  }, [building.code, building.floors, eateryData, searchMap]);
-
   const renderBuildingImage = () => {
     const url = `/assets/location_images/building_room_images/${building.code}/${building.code}.jpg`;
 
@@ -80,88 +43,109 @@ const BuildingCard = ({ map, building }: Props) => {
     };
 
     if (isMobile) {
-      const responsive = {
-        superLargeDesktop: {
-          breakpoint: { max: 4000, min: 3000 },
-          items: 5,
-        },
-        desktop: {
-          breakpoint: { max: 3000, min: 1024 },
-          items: 3,
-        },
-        tablet: {
-          breakpoint: { max: 1024, min: 464 },
-          items: 2,
-        },
-        mobile: {
-          breakpoint: { max: 464, min: 0 },
-          items: 1,
-          partialVisibilityGutter: 30,
-        },
-      };
+      return;
+      // const responsive = {
+      //   superLargeDesktop: {
+      //     breakpoint: { max: 4000, min: 3000 },
+      //     items: 5,
+      //   },
+      //   desktop: {
+      //     breakpoint: { max: 3000, min: 1024 },
+      //     items: 3,
+      //   },
+      //   tablet: {
+      //     breakpoint: { max: 1024, min: 464 },
+      //     items: 2,
+      //   },
+      //   mobile: {
+      //     breakpoint: { max: 464, min: 0 },
+      //     items: 1,
+      //     partialVisibilityGutter: 30,
+      //   },
+      // };
 
-      // eslint-disable-next-line @typescript-eslint/no-explicit-any
-      const CustomDot = ({ onClick, active }: any) => {
-        return (
-          <button
-            className={
-              'h-2 w-2 rounded-full ' +
-              `${active ? 'bg-[#8e8e8e]' : 'bg-[#f1f1f1]'}`
-            }
-            onClick={() => onClick()}
-          ></button>
-        );
-      };
+      // // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      // const CustomDot = ({ onClick, active }: any) => {
+      //   return (
+      //     <button
+      //       className={
+      //         'h-2 w-2 rounded-full ' +
+      //         `${active ? 'bg-[#8e8e8e]' : 'bg-[#f1f1f1]'}`
+      //       }
+      //       onClick={() => onClick()}
+      //     ></button>
+      //   );
+      // };
 
-      return (
-        <div className="mb-1 ml-2">
-          <p className="my-0 font-medium">Eateries nearby</p>
-          <Carousel
-            responsive={responsive}
-            showDots
-            arrows={false}
-            dotListClass="gap-2"
-            customDot={<CustomDot />}
-          >
-            {eatingData.map(([eatery, eateryInfo]) => (
-              <div
-                key={eatery.id}
-                className="cursor-pointer rounded border p-1"
-                onClick={() => dispatch(claimRoom(eatery))}
-              >
-                <div className="carousel-item active">
-                  <EateryInfoDisplay
-                    room={eatery}
-                    title={renderTitle(eatery)}
-                    eateryInfo={eateryInfo}
-                  />
-                </div>
-              </div>
-            ))}
-          </Carousel>
-        </div>
-      );
+      // return (
+      //   <div className="mb-1 ml-2">
+      //     <p className="my-0 font-medium">Eateries nearby</p>
+      //     <Carousel
+      //       responsive={responsive}
+      //       showDots
+      //       arrows={false}
+      //       dotListClass="gap-2"
+      //       customDot={<CustomDot />}
+      //     >
+      //       {eatingData.map(([eatery, eateryInfo]) => (
+      //         <div
+      //           key={eatery.id}
+      //           className="cursor-pointer rounded border p-1"
+      //           onClick={() => dispatch(claimRoom(eatery))}
+      //         >
+      //           <div className="carousel-item active">
+      //             <EateryInfoDisplay
+      //               room={eatery}
+      //               title={renderTitle(eatery)}
+      //               eateryInfo={eateryInfo}
+      //             />
+      //           </div>
+      //         </div>
+      //       ))}
+      //     </Carousel>
+      //   </div>
+      // );
     } else {
       const handleClick = (eatery: SearchRoom) => () => {
         dispatch(claimRoom(eatery));
         zoomOnSearchRoom(map, eatery, buildings, floorPlanMap, dispatch);
       };
 
+      const eateries = building.floors
+        .map((floorLevel) => {
+          // remove this later!!!
+          if (
+            !searchMap[`${building.code}`] ||
+            !searchMap[`${building.code}`][floorLevel]
+          ) {
+            return [];
+          }
+          const rooms = searchMap[`${building.code}`][`${floorLevel}`];
+          return rooms.filter((room) => room.type == 'food');
+        })
+        .flat();
+
+      sortEateries(eateries, eateryData);
+
       return (
         <div className="mx-2 mb-3 max-h-96 space-y-3 overflow-y-auto">
-          {eatingData.map(([eatery, eateryInfo]) => (
-            <div
-              key={eatery.id}
-              className="cursor-pointer rounded border p-1"
-              onClick={handleClick(eatery)}
-            >
-              <EateryInfoDisplay
-                room={eatery}
-                title={renderTitle(eatery)}
-                eateryInfo={eateryInfo}
-              />
-            </div>
-          ))}
+          {eateries.map((eatery) => {
+            const eateryInfo = eateryData[eatery.alias.toUpperCase()];
+
+            return (
+              <div
+                key={eatery.id}
+                className="cursor-pointer rounded border p-1"
+                onClick={handleClick(eatery)}
+              >
+                <EateryInfoDisplay
+                  room={eatery}
+                  title={renderTitle(eatery)}
+                  eateryInfo={eateryInfo}
+                />
+              </div>
+            );
+          })}
         </div>
       );
     }
