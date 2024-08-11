ba1e608a21e5bf74773285fded4eb17b5b40dcfe
Yuxiang-Huang <yuxiangh142857@gmail.com>
2024-08-11 16:00:31 -0400

    More fixing on the course search results...

--- src/components/searchbar/CourseSearchResults.tsx
+++ src/components/searchbar/CourseSearchResults.tsx
@@ -1,8 +1,13 @@
+import courseIcon from '@icons/quick_search/course.svg';
+import Image from 'next/image';
+
 import React, { useEffect, useState } from 'react';
 
 import { setCourseData } from '@/lib/features/dataSlice';
 import { useAppDispatch, useAppSelector } from '@/lib/hooks';
 import { AbsoluteCoordinate, Course, CourseData } from '@/types';
+
+import SearchResultWrapper from './SearchResultWrapper';
 
 interface Props {
   map: mapkit.Map | null;
@@ -18,27 +23,31 @@ const CourseSearchResults = ({ query }: Props) => {
 
   // load course data
   useEffect(() => {
-    fetch('/json/courses.json').then((response) =>
-      response.json().then((data) => {
-        dispatch(setCourseData(data));
-      }),
-    );
-  }, [dispatch]);
+    if (!courseData) {
+      fetch('/json/courses.json').then((response) =>
+        response.json().then((data) => {
+          dispatch(setCourseData(data));
+        }),
+      );
+    }
+  }, [courseData, dispatch]);
 
   // search courses
   useEffect(() => {
-    if (query.length < 2) {
+    if (!courseData) {
       return;
     }
+
+    const loweredQuery = query.toLowerCase();
 
     const newSearchResults: CourseData = {};
 
     Object.entries(courseData).map(([department, courses]) => {
       Object.entries(courses).map(([courseCode, course]: [string, Course]) => {
         if (
-          department.includes(query) ||
-          courseCode.includes(query) ||
-          course.name.includes(query)
+          department.toLowerCase().includes(loweredQuery) ||
+          courseCode.toLowerCase().includes(loweredQuery) ||
+          course.name.toLowerCase().includes(loweredQuery)
         ) {
           if (!newSearchResults[department]) {
             newSearchResults[department] = {};
@@ -51,13 +60,43 @@ const CourseSearchResults = ({ query }: Props) => {
     setSearchResult(newSearchResults);
   }, [courseData, query]);
 
-  if (query.length < 2) {
-    return <p>Type more pls</p>;
-  }
+  const renderCourseResultHelper = (courseCode: string, course: Course) => {
+    return Object.entries(course.sections).map(([sectionCode, section]) => (
+      <div key={courseCode} className="flex items-center gap-2 py-1 text-left">
+        <div className="flex h-12 w-12 items-center justify-center rounded-full bg-red-700">
+          <Image alt={'Event Icon'} src={courseIcon} />
+        </div>
+        <SearchResultWrapper
+          key={courseCode + sectionCode}
+          handleClick={() => {
+            console.log('later');
+          }}
+        >
+          <>
+            <p className="truncate">
+              {courseCode} {sectionCode}
+            </p>
+            <p>{course.name}</p>
+            <p>
+              {section.dow} {section.startTime} {section.endTime}
+            </p>
+            <p>{section.room}</p>
+          </>
+        </SearchResultWrapper>
+      </div>
+    ));
+  };
 
   return Object.entries(searchResult).map(([department, courses]) => (
     <div key={department}>
-      <p>{department}</p>
+      <SearchResultWrapper>
+        <h3>{department}</h3>
+      </SearchResultWrapper>
+      {Object.entries(courses)
+        .slice(0, 10)
+        .map(([courseCode, course]) =>
+          renderCourseResultHelper(courseCode, course),
+        )}
     </div>
   ));
 };
