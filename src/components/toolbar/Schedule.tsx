e7ff766f9a2d105a9d9b8f265481c964665b40a8
Yuxiang-Huang <yuxiangh142857@gmail.com>
2024-08-12 15:08:43 -0400

    UI done for schedule calendar upload

--- src/components/toolbar/Schedule.tsx
+++ src/components/toolbar/Schedule.tsx
@@ -3,12 +3,40 @@ import React from 'react';
 import CollapsibleWrapper from '../common/CollapsibleWrapper';
 
 const Schedule = () => {
+  const handleFileChange = (event) => {
+    const file = event.target.files[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.readAsText(file);
+
+      reader.onload = (e) => {
+        console.log(e.target.result);
+      };
+    }
+  };
+
   return (
     <CollapsibleWrapper title="Schedule">
-      <div className="px-5 pb-1">
-        <p>First step: SIO</p>
+      <div className="space-y-2 px-5 pb-2">
         <p>
-          Second step: Wait until tomorrow for the release of the feature...
+          First step: download{' '}
+          <a
+            target="_blank"
+            rel="noreferrer"
+            href="https://s3.andrew.cmu.edu/sio/mpa/secure/export/schedule/F24_schedule.ics"
+          >
+            Calendar Export
+          </a>{' '}
+          from SIO
+        </p>
+        <p>
+          Second step: Import the .ics file here:
+          <input
+            type="file"
+            id="fileInput"
+            accept=".ics"
+            onChange={handleFileChange}
+          />
         </p>
       </div>
     </CollapsibleWrapper>
