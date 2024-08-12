1700a46d2aacbca2570d1badd1e291fd17aae486
Yuxiang-Huang <yuxiangh142857@gmail.com>
2024-08-11 23:34:35 -0400

    Fixed eatery types

--- scripts/get_all_eaterys.py
+++ scripts/get_all_eaterys.py
@@ -10,13 +10,14 @@ response = requests.get(url)
 for eatery in response.json()["locations"]:
     eatery_names.append(eatery["name"])
 
-for root, dirs, files in os.walk("public/json"):
+for root, dirs, files in os.walk("public/json/floor_plan"):
     for file in files:
         file_path = os.path.join(root, file)
         if "outline" in file_path:
             with open(file_path, "r") as file:
                 content = json.loads(file.read())
                 if "placement" in content:
+                    print(file_path)
                     for room_id in content["rooms"]:
                         room = content["rooms"][room_id]
                         for alias in room["aliases"]:
@@ -24,4 +25,4 @@ for root, dirs, files in os.walk("public/json"):
                                 room["type"] = "food"
 
             with open(file_path, "w") as f:
-                json.dump(content, f, indent=4)
+                json.dump(content, f)
