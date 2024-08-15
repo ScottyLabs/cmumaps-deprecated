2ac37d780da2f2986dbbd95b28d311beb1119f49
Yuxiang-Huang <yuxiangh142857@gmail.com>
2024-08-15 00:35:50 -0400

    Used placements.json to combine the floor plans

--- scripts/combine_all_floor_plans.py
+++ scripts/combine_all_floor_plans.py
@@ -68,6 +68,9 @@ def simplify_polygon(polygon):
     return simplify(shape(polygon), tolerance=5)
 
 
+with open("public/json/placements.json", "r") as file:
+    placements = json.loads(file.read())
+
 for root, dirs, files in os.walk("public/json/floor_plan"):
     building_code = root.split("/")[-1]
 
@@ -77,46 +80,58 @@ for root, dirs, files in os.walk("public/json/floor_plan"):
     floor_plan_map[building_code] = dict()
     for file in files:
         file_path = os.path.join(root, file)
+        building_code = root.split("/")[-1]
+
+        if "floor_plan" in building_code:
+            continue
+
         if "outline" in file_path:
             with open(file_path, "r") as file:
                 floor_level = file_path.split("-")[1]
                 content = json.loads(file.read())
-                if "placement" in content:
-                    placement = content["placement"]
+                if building_code not in placements:
+                    print(building_code + " is missing!")
+                    continue
 
-                    rooms = content["rooms"]
-                    floor_center = get_floor_center(rooms)
+                if floor_level not in placements[building_code]:
+                    print(building_code + " " + floor_level + " is missing!")
+                    continue
 
-                    for room_id in content["rooms"]:
-                        room = content["rooms"][room_id]
-                        room["id"] = room_id
-                        room["labelPosition"] = position_on_map(
-                            room["labelPosition"], placement, floor_center
-                        )
+                placement = placements[building_code][floor_level]
 
-                        room["floor"] = dict()
-                        room["floor"]["buildingCode"] = building_code
-                        room["floor"]["level"] = floor_level
+                rooms = content["rooms"]
+                floor_center = get_floor_center(rooms)
 
-                        # room["polygon"] = json.loads(
-                        #     to_geojson(simplify_polygon(rooms[room_id]["polygon"]))
-                        # )
+                for room_id in content["rooms"]:
+                    room = content["rooms"][room_id]
+                    room["id"] = room_id
+                    room["labelPosition"] = position_on_map(
+                        room["labelPosition"], placement, floor_center
+                    )
 
-                        new_coordinates = []
-                        for ring in room["polygon"]["coordinates"]:
-                            new_ring = []
-                            for coordinate in ring:
-                                new_ring.append(
-                                    position_on_map(
-                                        {"x": coordinate[0], "y": coordinate[1]},
-                                        placement,
-                                        floor_center,
-                                    )
+                    room["floor"] = dict()
+                    room["floor"]["buildingCode"] = building_code
+                    room["floor"]["level"] = floor_level
+
+                    # room["polygon"] = json.loads(
+                    #     to_geojson(simplify_polygon(rooms[room_id]["polygon"]))
+                    # )
+
+                    new_coordinates = []
+                    for ring in room["polygon"]["coordinates"]:
+                        new_ring = []
+                        for coordinate in ring:
+                            new_ring.append(
+                                position_on_map(
+                                    {"x": coordinate[0], "y": coordinate[1]},
+                                    placement,
+                                    floor_center,
                                 )
-                            new_coordinates.append(new_ring)
-                        room["coordinates"] = new_coordinates
+                            )
+                        new_coordinates.append(new_ring)
+                    room["coordinates"] = new_coordinates
 
-                    floor_plan_map[building_code][floor_level] = content["rooms"]
+                floor_plan_map[building_code][floor_level] = content["rooms"]
 
 with open("public/json/floorPlanMap.json", "w") as file:
     file.write(json.dumps(floor_plan_map))
