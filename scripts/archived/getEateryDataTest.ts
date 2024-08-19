dfa789fd446ef18ef529585af93957e56d1999c5
Yuxiang-Huang <yuxiangh142857@gmail.com>
2024-08-18 22:06:09 -0400

    Archived python files

--- scripts/archived/getEateryDataTest.ts
+++ scripts/archived/getEateryDataTest.ts
@@ -1,5 +1,5 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { EateryInfo } from '../src/types';
+import { EateryInfo } from '../../src/types';
 
 const daysOfWeek = [
   'Sunday', // 0
