0ee5e0a4cd8dfcc6ffb46c152835e282a7641646
Theodor Urban <theo.s.urban@gmail.com>
2024-08-22 22:14:37 -0400

    Update PWA-ability

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,6 +12,7 @@ const lato = Lato({
 });
 
 // https://nextjs.org/docs/app/building-your-application/optimizing/metadata
+
 export const metadata: Metadata = {
   title: 'CMU Maps',
   description: 'Interactive map of the CMU campus',
@@ -29,6 +30,20 @@ export default function RootLayout({
       >
         <html lang="en" className={lato.className}>
           <head>
+            {/* For PWA, next does not like these */}
+            <meta name="mobile-web-app-capable" content="yes" />
+            <meta name="apple-mobile-web-app-capable" content="yes" />
+            <meta
+              name="apple-mobile-web-app-status-bar-style"
+              content="black-translucent"
+            />
+            <meta
+              name="viewport"
+              content="width=device-width, initial-scale=1"
+            />
+            <meta name="theme-color" content="#000000" />
+            <meta name="color-scheme" content="dark" />
+            <meta name="description" content="Google Maps for CMU" />
             <script>const global = globalThis;</script>
           </head>
 
