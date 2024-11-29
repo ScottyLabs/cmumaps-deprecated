83e7db24fbf5ef685ca16645899336bd5a1bb920
Theodor Urban <theo.s.urban@gmail.com>
2024-11-29 14:40:53 -0500

    Delete unused code

--- src/app/[[...slug]]/page.tsx
+++ src/app/[[...slug]]/page.tsx
@@ -75,24 +75,6 @@ const Page = ({ params, searchParams }: Props) => {
   const startLocation = useAppSelector((state) => state.nav.startLocation);
   const endLocation = useAppSelector((state) => state.nav.endLocation);
   const userPosition = useAppSelector((state) => state.nav.userPosition);
-  const registerServiceWorker = async () => {
-    if ('serviceWorker' in navigator) {
-      try {
-        const registration = await navigator.serviceWorker.register('/sw.js', {
-          scope: '/',
-        });
-        if (registration.installing) {
-          console.log('Service worker installing');
-        } else if (registration.waiting) {
-          console.log('Service worker installed');
-        } else if (registration.active) {
-          console.log('Service worker active');
-        }
-      } catch (error) {
-        console.error(`Registration failed with ${error}`);
-      }
-    }
-  };
   useEffect(() => {
     if ('serviceWorker' in navigator) {
       window.addEventListener('load', () => {
