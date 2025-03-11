ca623cea22e9919ebc4fee44522ecc25513174e8
Theodor Urban <theo.s.urban@gmail.com>
2025-03-10 23:05:23 -0400

    fix headers on findpath

--- src/components/navigation/NavCard.tsx
+++ src/components/navigation/NavCard.tsx
@@ -55,15 +55,13 @@ const NavCard = ({ map }: Props) => {
     if (startLocation && endLocation) {
       dispatch(setRecommendedPath(null));
       fetch(
-        process.env.NEXT_PUBLIC_FINDPATH_ENDPOINT +
+        process.env.NEXT_PUBLIC_SEARCH_ENDPOINT +
           process.env.NODE_ENV +
-          '/find-path',
+          '/alpha-search',
         {
           method: 'POST',
-
           headers: {
-            Authorization: `Bearer ${token}`,
-            'Content-Type': 'application/json',
+            authorization: `Bearer ${token}`,
           },
           body: JSON.stringify({ waypoints: [startLocation, endLocation] }),
         },
